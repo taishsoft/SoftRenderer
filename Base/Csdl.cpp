78d5d25a5162c4a8c28a7d1251362d2e9913052a
MOKUN\lvqiang.coco <lvqiang.coco@bytedance.com>
2021-09-30 10:11:23 +0800

    sdl封装

--- Base/Csdl.cpp
+++ Base/Csdl.cpp
@@ -1,2 +1,49 @@
 #include "Csdl.h"
 
+CSDL::CSDL()
+{
+
+}
+
+CSDL::CSDL(int width, int height, const char* windowName)
+{
+	screenWidth = width;
+	screenHeight = height;
+
+	//Initialize SDL
+	if (SDL_Init(SDL_INIT_VIDEO) < 0)
+	{
+		CheckError("SDL could not initialize!");
+	}
+	else
+	{
+		//Set texture filtering to linear
+		if (!SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "1"))
+		{
+			printf("Warning: Linear texture filtering not enabled!");
+		}
+
+		//Create window
+		mainWindow = SDL_CreateWindow("SoftRenderer", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+		if (mainWindow == NULL)
+		{
+			CheckError("Window could not be created!");
+		}
+		else
+		{
+			//Create renderer for window
+			renderer = SDL_CreateRenderer(mainWindow, -1, SDL_RENDERER_ACCELERATED);
+			if (renderer == NULL)
+			{
+				CheckError("Renderer could not be created! ");
+			}
+		}
+	}
+}
+
+void CSDL::CheckError(const char* msg)
+{
+	std::cout << ("%s : %s\n", msg, SDL_GetError()) << std::endl;
+	SDL_Quit();
+	exit(1);
+}
--- Base/Csdl.h
+++ Base/Csdl.h
@@ -1,6 +1,7 @@
 ﻿#pragma once
 #include <SDL.h>
-
+#include <iostream>
+#include "Color.h"
 
 class RenderContext;
 
@@ -16,10 +17,28 @@ public:
 	~CSDL();
 
 	void Render();
-	void Clean(RenderContext* renderContext);
+	void Clean(RenderContext* renderContext, Color color);
 	void Quit(int code);
 	void ToggleFullscreen();
 	void HandleKeyEvent(SDL_Keysym* keysym);
 	void HandleEvents();
+	void CheckError(const char* msg);
+	void CheckSDLError(int line = -1);
+	void SetDrawColor(Color color);
+
+	void DrawPixel(int x, int y);
+	void SwapBuffer(RenderContext* renderContext);
+
+private:
+	SDL_Window* mainWindow;
+	SDL_Renderer* renderer;
+	SDL_Surface* surface;
+	SDL_Texture* mainRT;
+
+	Uint32 rmask, gmask, bmask, amask;
+
+	bool windowed;
+
+	bool windowOK;
 };
 
